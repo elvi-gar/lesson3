e617caea1707e0e4e1a45bd07b082d24682fbb8a
elvi-gar <82706784+elvi-gar@users.noreply.github.com>
2021-10-27 15:56:06 +0300

    Update Orange.java

--- Orange.java
+++ Orange.java
@@ -1,16 +1,1 @@
-package lesson_3;
 
-//Апельсины
-
-public class Orange extends Fruit {
-
-    Orange() {
-        super(1.5f);
-    }
-
-    @Override
-    public String toString(){
-        return "апельсины";
-    }
-
-}
