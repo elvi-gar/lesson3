45001c28e0008cfebceedfae9160fa8b2462824e
elvi-gar <82706784+elvi-gar@users.noreply.github.com>
2021-10-27 15:55:16 +0300

    Update Fruit.java

--- Fruit.java
+++ Fruit.java
@@ -1,15 +1,1 @@
-package lesson_3;
 
-//Фрукты
-
-public class Fruit {
-    private float weight;
-
-    Fruit(float weight) {
-        this.weight = weight;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-}
