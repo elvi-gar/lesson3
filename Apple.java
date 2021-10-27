eeb62cb30184d4ef17872aa088b3be443380d019
elvi-gar <82706784+elvi-gar@users.noreply.github.com>
2021-10-27 15:44:07 +0300

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -1,13 +1,2 @@
 package lesson_3;
 
-public class Apple extends Fruit {
-
-    Apple() {
-        super(1.0f);
-    }
-
-    @Override
-    public String toString(){
-        return "яблоки";
-    }
-}
